8414cef29505b5eb7b8c5a0c26cb303e63951743
xujin <Software_King@qq.com>
2018-07-05 23:05:21 +0800

    去除删除多余的类信息

--- crm-sales-app/src/main/java/org/xujin/crm/sales/validator/extensionpoint/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Validator Extension Point are used to declare the point needs extension per different tenant.
- * 
- * @author xujin
- */
-package org.xujin.crm.sales.validator.extensionpoint;
